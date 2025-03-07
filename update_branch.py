9a28986ec60a824d246a3e15c033c7b74350f0a3
Daniel <danielbetzalel16@gmail.com>
2025-03-07 17:34:40 +0200

    try 6

--- update_branch.py
+++ update_branch.py
@@ -27,17 +27,30 @@ def update_main():
     run_command("git pull origin main")
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print("Usage: python update_branch.py [daniel|alpha|main]")
+    if len(sys.argv) < 2:
+        print("Usage: python update_branch.py [daniel|alpha|main|add_commit_push]")
         sys.exit(1)
 
     task = sys.argv[1].lower()
     if task == "daniel":
         update_daniel()
+        
     elif task == "alpha":
         update_alpha()
+        
     elif task == "main":
         update_main()
+        
+    elif task == "add_commit_push":
+        add_argument = sys.argv[2]
+        print("add_argument=",add_argument)
+        commit_message = " ".join(sys.argv[3:])  # Join all remaining arguments
+        # Validate that neither argument is empty
+        print("commit_message=",commit_message)
+        if not add_argument.strip() or not commit_message.strip():
+            print("Error: Files name or Commit msg cannot be empty.")
+            sys.exit(1)
+        add_commit_push(add_argument, commit_message)
     else:
-        print("Invalid option. Please choose 'daniel', 'alpha', or 'main'.")
+        print("Invalid option.")
         sys.exit(1)
