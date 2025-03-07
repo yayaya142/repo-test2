24afe1fb60c7129a85058acd8a40bd24d4c4515d
Daniel <danielbetzalel16@gmail.com>
2025-03-07 18:22:08 +0200

    updated blah

--- update_branch.py
+++ update_branch.py
@@ -19,39 +19,91 @@ def add_commit_push(addArgument, commitMessage):# commit the changes in daniel b
     run_command('git commit -m "{}"'.format(commitMessage))
     run_command("git push origin daniel")
 
-def update_alpha(): # sync the alpha branch with github alpha branch
+
+
+def update_local_alpha(): # sync the alpha branch with github alpha branch
+
+
     run_command("git checkout alpha")
     run_command("git pull origin alpha")
 
-def update_main(): # sync the main branch with github main branch
+
+
+def update_local_main(): # sync the main branch with github main branch
+
+
     run_command("git checkout main")
     run_command("git pull origin main")
 
+
+
 if __name__ == '__main__':
+
+
     if len(sys.argv) < 2:
+
+
         print("Usage: python update_branch.py [daniel|alpha|main|add_commit_push]")
+
+
         sys.exit(1)
 
+
+
     task = sys.argv[1].lower()
+
+
     if task == "daniel":
         update_daniel()
         
+
+
     elif task == "alpha":
-        update_alpha()
+        update_local_alpha()
         
+
+
     elif task == "main":
-        update_main()
+        update_local_main()
         
+
+
     elif task == "add_commit_push":
+
+
         add_argument = sys.argv[2]
+
+
         print("add_argument=",add_argument)
+
+
         commit_message = " ".join(sys.argv[3:])  # Join all remaining arguments
+
+
         # Validate that neither argument is empty
+
+
         print("commit_message=",commit_message)
+
+
         if not add_argument.strip() or not commit_message.strip():
+
+
             print("Error: Files name or Commit msg cannot be empty.")
+
+
             sys.exit(1)
+
+
         add_commit_push(add_argument, commit_message)
+
+
     else:
+
+
         print("Invalid option.")
+
+
         sys.exit(1)
+
+
